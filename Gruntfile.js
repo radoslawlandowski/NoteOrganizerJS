f12106b2a944c6e3be5cb530de733e2be7fabb38
radoslawlandowski <radoslaw.landowski@gmail.com>
2016-12-27 15:19:44 +0100

    Improve grunt tasking

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,15 @@
 module.exports = function(grunt) {
 
-var testsFolderPath;
+  var testTypes = grunt.option('testTypes') || 'all';
+  var environment = grunt.option('environment') || 'development';
 
+  var generalTestOptions = {
+    reporter: 'mocha-junit-reporter',
+    captureFile: 'test-results.xml',
+    quiet: false,
+    clearRequireCache: false,
+    noFail: false
+  };
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -28,46 +36,22 @@ var testsFolderPath;
 
     mochaTest: {
       all: {
-        options: {
-          reporter: 'mocha-junit-reporter',
-          captureFile: 'test-results.xml',
-          quiet: false,
-          clearRequireCache: false,
-          noFail: false
-        },
+        options: generalTestOptions,
         src: ['test/**/*.js']
       },
 
       integration: {
-        options: {
-          reporter: 'mocha-junit-reporter',
-          captureFile: 'test-results.xml',
-          quiet: false,
-          clearRequireCache: false,
-          noFail: false
-        },
+        options: generalTestOptions,
         src: ['test/integration/**/*.js']
       },
 
       unit: {
-        options: {
-          reporter: 'mocha-junit-reporter',
-          captureFile: 'test-results.xml',
-          quiet: false,
-          clearRequireCache: false,
-          noFail: false
-        },
+        options: generalTestOptions,
         src: ['test/unit/**/*.js']
       },
 
       endtoend: {
-        options: {
-          reporter: 'mocha-junit-reporter',
-          captureFile: 'test-results.xml',
-          quiet: false,
-          clearRequireCache: false,
-          noFail: false
-        },
+        options: generalTestOptions,
         src: ['test/endtoend/**/*.js']
       }
     }
@@ -76,15 +60,10 @@ var testsFolderPath;
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-npm-install');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-env');
 
-
-  var testTypes = grunt.option('testTypes') || 'all';
-  var environment = grunt.option('environment') || 'development';
-
-  grunt.registerTask('default', ['env', 'clean', 'npm-install', 'copy', 'mochaTest:' + testTypes]);
+  grunt.registerTask('default', ['env', 'clean', 'copy', 'mochaTest:' + testTypes]);
   grunt.registerTask('test', ['env', 'mochaTest:' + testTypes])
 
 };
