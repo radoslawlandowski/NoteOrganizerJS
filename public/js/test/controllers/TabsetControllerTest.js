bb347647219831272804eac66dded4496153cbab
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-02-13 20:37:24 +0100

    Refactor TabsetController tests

--- public/js/test/controllers/TabsetControllerTest.js
+++ public/js/test/controllers/TabsetControllerTest.js
@@ -30,7 +30,8 @@
              {"_id":30,"title":"ToSchool","content":"ToSchool","date":null,"owner":null,"tab":"School"}
           ];
 
-          var noteToBeSent = notes[0];
+          var noteToBeSent = {"_id":34,"title":"ToSchool","content":"ToSchool","date":1472054586105,"owner":null,"tab":"School"};
+          var noteToBeDeleted = {"_id":28,"title":"EDITED","content":"EDITED","date":null,"owner":null,"tab":"main"};
 
           var tabs = [
              {"_id": 1, "name": "First"},
@@ -42,26 +43,28 @@
      describe('TabsetController:', function() {
          beforeEach(angular.mock.module('NoteOrganizerModule'));
 
-         var $controller, $state, $scope, noteService, q, tabsetController, deferred, tabService, dialogService;
-         beforeEach(inject(function(_$controller_, _$state_, _NoteService_, _TabService_, $q, _$rootScope_, _DialogService_){
+         var $controller, $state, $scope, $q;
+         var noteService, tabService, dialogService; 
+         var tabsetController; 
+         var deferredNoteService, deferredTabService, deferredDialogService;
+
+         beforeEach(inject(function(_$controller_, _$state_, _$q_, _$rootScope_, _DialogService_, _NoteService_, _TabService_){
              $controller = _$controller_;
              $state = _$state_;
              noteService = _NoteService_;
              dialogService = _DialogService_;
-             q = $q;
+             $q = _$q_;
              $scope = _$rootScope_.$new();
              tabService = _TabService_;
 
-             deferred = q.defer();
-             deferredTabService = q.defer();
-             deferredDialogService = q.defer();
+             deferredNoteService = $q.defer();
+             deferredTabService = $q.defer();
+             deferredDialogService = $q.defer();
 
-             spyOn(noteService, 'all').and.returnValue(deferred.promise);
-             spyOn(noteService, 'send').and.returnValue(deferred.promise);
-             spyOn(noteService, 'delete').and.returnValue(deferred.promise);
-
+             spyOn(noteService, 'all').and.returnValue(deferredNoteService.promise);
+             spyOn(noteService, 'send').and.returnValue(deferredNoteService.promise);
+             spyOn(noteService, 'delete').and.returnValue(deferredNoteService.promise);
              spyOn(tabService, 'all').and.returnValue(deferredTabService.promise);
-
              spyOn(dialogService, 'getDeleteNoteDialog').and.returnValue(deferredDialogService.promise);
 
              spyOn($state, 'go').and.stub();
@@ -69,10 +72,9 @@
              tabsetController = $controller('TabsetController', {NoteService: noteService, DialogService: dialogService, $state: $state, $scope: $scope, notes: notes, tabs: tabs});
          }));
 
-         describe('1. when controller has just been created', function() {
-             it('1.0. it have notes and tabs already initialized', function() {
-
-                 deferred.resolve(notes);
+         describe('When controller has just been created', function() {
+             it('it should have notes and tabs already initialized', function() {
+                 deferredNoteService.resolve(notes);
                  deferredTabService.resolve(tabs);
                  $scope.$apply();
 
@@ -82,8 +84,10 @@
 
          });
 
-         describe('2. when sendNote has been called:', function() {
-             it('2.0. it should call NoteService.create(note) and call $state.go()', function() {
+         describe('When sendNote has been called:', function() {
+             it('it should call NoteService.send(note) and call $state.go()', function() {
+                 var noteCount = $scope.notes.length;
+
                  tabsetController.sendNote(noteToBeSent);
                  expect(noteService.send).toHaveBeenCalledWith(noteToBeSent);
 
@@ -92,14 +96,19 @@
                      'message': 'asd'
                  };
 
-                 deferred.resolve(response);
+                 deferredNoteService.resolve(response);
                  $scope.$apply();
+
+                 expect($state.go).toHaveBeenCalledWith('tdp.notes');
+
+                 var noteCountAfter = $scope.notes.length;
+                 expect(noteCountAfter - noteCount).toEqual(1);
              });
          });
 
-         describe('3. when deleteNote has been called:', function() {
-             it('3.0. it should call NoteService.deleteNote(note) and call $state.go()', function() {
-                 tabsetController.deleteNote(noteToBeSent);
+         describe('When deleteNote has been called:', function() {
+             it('it should call NoteService.deleteNote(note) and call $state.go()', function() {
+                 tabsetController.deleteNote(noteToBeDeleted);
                  expect(dialogService.getDeleteNoteDialog).toHaveBeenCalled();
                  
                  deferredDialogService.resolve();
@@ -107,12 +116,12 @@
 
                  var noteCount = $scope.notes.length;
 
-                 expect(noteService.delete).toHaveBeenCalledWith(noteToBeSent);
+                 expect(noteService.delete).toHaveBeenCalledWith(noteToBeDeleted);
                  var response = {
-                     noteId: noteToBeSent._id
+                     noteId: noteToBeDeleted._id
                  };
 
-                 deferred.resolve(response);
+                 deferredNoteService.resolve(response);
                  $scope.$apply();
 
                  var noteCountAfter = $scope.notes.length;
